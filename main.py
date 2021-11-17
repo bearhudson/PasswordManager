9bbf602cb7e49befbc34c323711aa2dc716c5ce5
Brian Hudson <brian.hudson@flexential.com>
2021-11-16 21:46:12 -0500

    Framework

--- main.py
+++ main.py
@@ -3,41 +3,22 @@
 from tkinter import *
 from tkinter import ttk
 
+WHITE = "#ffffff"
+FONT_NAME = "Courier"
 
-class PasswordManager:
+window = Tk()
+window.title("Password Manager")
+window.config(padx=100, pady=50)
+title = Label(text='Password Manager', font=(FONT_NAME, 20, 'bold'))
+title.grid(row=0, column=1)
+canvas = Canvas(width=600, height=600, highlightthickness=0)
+lock_image = PhotoImage(file="padlock.png")
+canvas.create_image(300, 220, image=lock_image)
+canvas.grid(row=1, column=1)
+password_text = Label(text='Password: ', font=(FONT_NAME, 12, 'bold'))
+password_text.grid(row=2, column=0)
+password_entry = Entry(width=50)
+password_entry.insert(END, string='Passwords')
+password_entry.grid(row=2, column=1)
 
-    def __init__(self, root):
-        root.title("Password Manager")
-
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0)
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
-
-        self.password = StringVar()
-        password_entry = ttk.Entry(mainframe, width=7, textvariable=self.password)
-        password_entry.grid(column=2, row=1, sticky=(W, E))
-
-        self.kilometers = StringVar()
-        ttk.Label(mainframe, textvariable=self.kilometers).grid(column=2, row=2, sticky=(W, E))
-
-        ttk.Button(mainframe, text="Save", command=self.save).grid(column=3, row=3, sticky=W)
-        ttk.Label(mainframe, text="Password").grid(column=3, row=1, sticky=W)
-
-        for child in mainframe.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-
-        password_entry.focus()
-        root.bind("<Return>", self.save)
-
-    def save(self, *args):
-        try:
-            value = self.password.get()
-            print(value)
-        except ValueError:
-            pass
-
-
-root = Tk()
-PasswordManager(root)
-root.mainloop()
+window.mainloop()
