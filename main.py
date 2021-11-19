2341e643a0ccdf89caaf12340f55d19f00784ba3
Brian Hudson <brian.hudson@flexential.com>
2021-11-19 16:24:26 -0500

    Finishing this madness

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from tkinter import ttk
 from tkinter.messagebox import showerror, askokcancel
 import random
 import string
-import pandas
 import os
 import pyperclip
 import json
@@ -16,23 +15,25 @@ password_len = 20
 
 
 class PasswordManager():
-    def __init__(self, root, password_list_df):
+    def __init__(self, root):
         # setup randoms for password generation
         self.char_string = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
         # setup windows
-        root.title("Aniki's Password Manager")
+        root.title("Brian's Password Manager")
         self.mainframe = ttk.Frame(root, padding='50 50 50 50')
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         self.title = Label(text='YAPM:', font=(FONT_NAME, 20, 'bold'))
         self.title.grid(row=0, column=0)
-        self.title2 = Label(text='Aniki\'s Yet Another Password Manager', font=(FONT_NAME, 12, 'bold'))
+        self.title2 = Label(text="Brian's Yet Another Password Manager", font=(FONT_NAME, 12, 'bold'))
         self.title2.grid(row=0, column=1, columnspan=2)
         self.generate_button = ttk.Button(root, text="Generate", command=self.generate)
         self.generate_button.grid(row=3, column=3)
         self.save_button = ttk.Button(root, text="Save", command=self.save_password)
         self.save_button.grid(row=4, column=3)
+        self.show_button = ttk.Button(root, text="Show", command=self.show_password)
+        self.show_button.grid(row=5, column=3)
         self.site_entry = Entry(width=40)
         self.site_entry.insert(0, string="")
         self.site_entry.grid(row=3, column=1, columnspan=2)
@@ -47,7 +48,7 @@ class PasswordManager():
         for site in self.get_site_list():
             self.url_list.insert(self.site_list_index, site)
             self.site_list_index += 1
-        self.url_list.bind("<<ListboxSelect>>", self.listbox_select)
+        self.url_list.bind("<<ListboxSelect>>", lambda event: self.listbox_select(self.url_list.get(self.url_list.curselection())))
         self.url_list.grid(row=6, column=0, columnspan=5)
         self.website_label = Label(text="Website    ▶").grid(row=3, column=0)
         self.login_label = Label(text="Login        ▶").grid(row=4, column=0)
@@ -58,6 +59,9 @@ class PasswordManager():
 
         self.password_entry.focus()
         root.bind("<Return>", self.generate)
+
+    def popup_error(self, error_string):
+        showerror(title="Error", message=f"Error: {error_string}")
 
     def generate(self):
         global password_len
@@ -71,6 +75,11 @@ class PasswordManager():
         pyperclip.copy(self.new_pw)
         self.password_entry.delete(0, END)
         self.password_entry.insert(0, string=self.new_pw)
+
+    def get_site_list(self):
+        with open("data.json", "r") as json_file:
+            self.json_file_data = json.load(json_file)
+        return self.json_file_data.keys()
 
     def save_password(self):
         self.current_site = self.site_entry.get()
@@ -86,21 +95,16 @@ class PasswordManager():
             self.popup_error("Please enter all three fields")
         else:
             is_ok = askokcancel(title=self.current_site,
-                                message=f"Press OK to continue:\n"
+                                message=f"Press OK to save.\n\n"
                                         f"Site: {self.current_site}\n"
                                         f"Login: {self.current_login}\n"
-                                        f"Password: {self.current_password}")
+                                        f"PW: {self.current_password}")
             if is_ok:
-                self.new_df = pandas.DataFrame({
-                    'site': [self.current_site],
-                    'login': [self.current_login],
-                    'password': [self.current_password]
-                })
-                self.pw_list = self.new_df.to_csv('password_list.csv', mode='a', index=False, header=False)
                 try:
                     with open("data.json", "r") as json_file:
                         self.json_file_data = json.load(json_file)
                 except FileNotFoundError:
+                    self.popup_error("File not found. Creating!")
                     with open("data.json", "a") as json_file:
                         json.dump(self.current_data_json, json_file, indent=4)
                 else:
@@ -111,17 +115,24 @@ class PasswordManager():
                     self.python = sys.executable
                     os.execl(self.python, self.python, * sys.argv)
 
-    def get_site_list(self):
-        return password_list_df['site'].to_list()
+    def listbox_select(self, listbox_select_site):
+        with open("data.json", "r") as json_file:
+            self.json_file_data = json.load(json_file)
+        print(type(self.json_file_data))
+        if listbox_select_site in self.json_file_data:
+            login = self.json_file_data[listbox_select_site]['login']
+            password = self.json_file_data[listbox_select_site]['password']
+        self.site_entry.delete(0, END)
+        self.site_entry.insert(0, listbox_select_site)
+        self.login_entry.delete(0, END)
+        self.login_entry.insert(0, login)
+        self.password_entry.delete(0, END)
+        self.password_entry.insert(0, password)
 
-    def listbox_select(self, obj):
-        print(obj)
-
-    def popup_error(self, error_string):
-        showerror(title="Error", message=f"Error: {error_string}")
+    def show_password(self):
+        pass
 
 
-password_list_df = pandas.read_csv('password_list.csv')
 root = Tk()
-pm = PasswordManager(root, password_list_df)
+pm = PasswordManager(root)
 root.mainloop()
